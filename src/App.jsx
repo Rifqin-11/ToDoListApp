0f809b033ed5aa7c8d2b5caefa41a2f50a8e4c98
Rifqi Naufal <rifqinaufal9009@gmail.com>
2024-11-01 16:24:39 +0700

    Hash Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import TaskList from './pages/TaskList';
 import TaskDetails from './components/TaskDetails';
 import NewTaskModal from './components/NewTaskModal';
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ToDoListApp/', // Add your repo name here
+  base: '/ToDoListApp/', // Replace 'ToDoListApp' with your actual repository name
 });
